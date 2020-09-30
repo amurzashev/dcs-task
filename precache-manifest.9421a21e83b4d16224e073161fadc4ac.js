473c893edcd9b3c119d3c41f368de40e7921fcaf
Abylay Murzashev <abylay.murzashev@gmail.com>
2020-09-30 19:14:33 +0600

    Updates

---
+++ precache-manifest.9421a21e83b4d16224e073161fadc4ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9267cf618b90b5b05909ba9b1b5367ba",
+    "url": "/test-assignment/index.html"
+  },
+  {
+    "revision": "d47a21d0d7cc2bb6c08d",
+    "url": "/test-assignment/static/css/2.87fd8954.chunk.css"
+  },
+  {
+    "revision": "d47a21d0d7cc2bb6c08d",
+    "url": "/test-assignment/static/js/2.c8429cf3.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/test-assignment/static/js/2.c8429cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc37330ecd7d25dd8b7",
+    "url": "/test-assignment/static/js/main.58d427e9.chunk.js"
+  },
+  {
+    "revision": "17952930f1aed8650f97",
+    "url": "/test-assignment/static/js/runtime-main.4063537b.js"
+  }
+]);
